de424ece13cc6833e4e444df0dbc4eeb9516dddc
Iyanu <oyerindei13@gmail.com>
2024-10-04 19:22:19 +0100

    Removed all "leading" classNames

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -103,7 +103,7 @@ export default function LandingPage() {
                 />
 
                 <div className="flex flex-col py-[2.5em]">
-                  <h2 className="poppins  text-[2.2em] md:text-[1.85em] font-semibold md:leading-10 lg:text-[2.4em] lg:leading-[1.3em] ">
+                  <h2 className="poppins  text-[2.2em] md:text-[1.85em] font-semibold lg:text-[2.4em] ">
                     The Card to support your community{" "}
                     <span className="text-[#FFB539]">with every swipe!</span>
                   </h2>
@@ -132,11 +132,11 @@ export default function LandingPage() {
                 alt="image"
               />
 
-              <h3 className="mt-[-2em] text-center text-[1.6em] font-semibold leading-8 ">
+              <h3 className="mt-[-2em] text-center text-[1.6em] font-semibold ">
                 The <span className="text-[#FF5810]">community</span> owned
                 card!
               </h3>
-              <p className="mb-2 mt-[1em] text-center text-[0.95em] leading-5">
+              <p className="mb-2 mt-[1em] text-center text-[0.95em] ">
                 Your Defy Card is by DAOs, for DAOs. Unlike centralized crypto
                 cards, you have a say in how we build the future.
               </p>
@@ -151,11 +151,11 @@ export default function LandingPage() {
                 width={1}
                 alt="image"
               />
-              <h3 className="mt-2 text-center text-[1.6em] font-semibold leading-8 ">
+              <h3 className="mt-2 text-center text-[1.6em] font-semibold ">
                 Support your <span className="text-[#13B5EC]">favorite</span>{" "}
                 communities
               </h3>
-              <p className="mb-2 mt-[1em] text-center text-[0.95em] leading-5">
+              <p className="mb-2 mt-[1em] text-center text-[0.95em] ">
                 Every time you use a card, 1-2% of the purchase price goes to a
                 bank. Bankless Card redirects some of that fee to your favorite
                 community.
@@ -171,11 +171,11 @@ export default function LandingPage() {
                 width={1}
                 alt="image"
               />
-              <h3 className="mt-2 text-center text-[1.6em] font-semibold leading-8 ">
+              <h3 className="mt-2 text-center text-[1.6em] font-semibold ">
                 Easily
                 <span className="text-[#27FFA4]"> onboard</span> new members
               </h3>
-              <p className="mb-2 mt-[1em] text-center text-[0.95em] leading-5">
+              <p className="mb-2 mt-[1em] text-center text-[0.95em] ">
                 With our easy-to-use mobile app, you can onboard new friends
                 into your community with a simple download.
               </p>
@@ -194,10 +194,10 @@ export default function LandingPage() {
                 alt="image"
               />
 
-              <h3 className="mt-2 text-center text-[1.65em] font-semibold leading-9 ">
+              <h3 className="mt-2 text-center text-[1.65em] font-semibold ">
                 Download the App
               </h3>
-              <p className="mb-2 mt-[1em] text-center text-[1em] text-[#9AA0A6] leading-6">
+              <p className="mb-2 mt-[1em] text-center text-[1em] text-[#9AA0A6]">
               Get a physical/virtual card, choose a community
               </p>
             </div>
@@ -211,11 +211,11 @@ export default function LandingPage() {
                 width={150}
                 alt="image"
               />
-              <h3 className="mt-2 text-center text-[1.65em]  font-semibold leading-9 ">
+              <h3 className="mt-2 text-center text-[1.65em]  font-semibold ">
               Make purchases, get token rewards
 
               </h3>
-              <p className="mb-2 mt-[1em] text-center text-[1em] text-[#9AA0A6] leading-6">
+              <p className="mb-2 mt-[1em] text-center text-[1em] text-[#9AA0A6]">
               50% of net interchange goes to chosen community
               </p>
             </div>
@@ -229,10 +229,10 @@ export default function LandingPage() {
                 width={150}
                 alt="image"
               />
-              <h3 className="mt-2 text-center text-[1.65em] font-semibold leading-9 ">
+              <h3 className="mt-2 text-center text-[1.65em] font-semibold ">
               Support your community
               </h3>
-              <p className="mb-2 mt-[1em] text-center text-[1em] text-[#9AA0A6] leading-6">
+              <p className="mb-2 mt-[1em] text-center text-[1em] text-[#9AA0A6] ">
               {`User’s community enjoys increased economic stability and health`}
               </p>
             </div>
@@ -276,7 +276,7 @@ export default function LandingPage() {
                     width={1}
                     alt=""
                   />
-                  <h3 className="text-[1.4em] mt-4 font-semibold leading-8">Download the App</h3>
+                  <h3 className="text-[1.4em] mt-4 font-semibold ">Download the App</h3>
                   <p className="text-[0.95em] mt-2 max-w-[80%] mx-auto">Get a physical/virtual card, choose a community</p>
                 </div>
 
@@ -288,7 +288,7 @@ export default function LandingPage() {
                     width={1}
                     alt=""
                   />
-                  <h3 className="text-[1.4em]  font-semibold leading-8 max-w-[85%] mx-auto">Make purchases, get token rewards
+                  <h3 className="text-[1.4em]  font-semibold max-w-[85%] mx-auto">Make purchases, get token rewards
                   </h3>
                   <p className="text-[0.95em] mt-2 max-w-[80%] mx-auto">50% of net interchange goes to chosen community</p>
                 </div>
@@ -301,7 +301,7 @@ export default function LandingPage() {
                     width={1}
                     alt=""
                   />
-                  <h3 className="text-[1.4em] mt-4 font-semibold leading-8">Support your community</h3>
+                  <h3 className="text-[1.4em] mt-4 font-semibold ">Support your community</h3>
                   <p className="text-[0.95em] mt-2 max-w-[80%] mx-auto">{`User’s community enjoys increased economic stability and health`}</p>
                 </div>
               </div>
@@ -312,7 +312,7 @@ export default function LandingPage() {
           <div className="grid w-[110%]">
           
             <div className="grid place-items-center text-center w-[90%] mx-auto pt-[8em] z-40">
-              <h2 className="text-[3em] mb-3 max-w-[62%] font-semibold leading-[1.3em]">Give your community a way to support your cause  </h2>
+              <h2 className="text-[3em] mb-3 max-w-[62%] font-semibold ">Give your community a way to support your cause  </h2>
               <p className="md:max-w-[35%] mt-[2em] mb-[3.4em] text-[#9AA0A6] text-[1em]">BCard works for every community, no matter how big or small, whether or not you have a token. </p>
             </div>
           </div>
@@ -323,7 +323,7 @@ export default function LandingPage() {
           <div className="flex flex-col-reverse max-sm:w-[92%] w-[90%] md:grid md:place-items-center md:grid-cols-[1fr,1fr] h-[fit-content] mx-auto md:h-[5em] mt-[2em] md:mx-auto w-[70%] gap-[5em] md:gap-[6em]">
             <div className="p-5 shadow-md gradient-bg-blue-purple rounded-xl h-[100%] w-[100%]">
               <div className="flex flex-col-reverse md:grid md:grid-cols-[70%,30%] pt-4 gap-2">
-                <h2 className="text-[1.6em] md:text-[1.4em] max-sm:mt-[-1.8em] md:px-4 font-semibold leading-8">{`Bcard powers your community’s `}{<span className="text-[#08BFFF]">economic</span>} engine</h2>
+                <h2 className="text-[1.6em] md:text-[1.4em] max-sm:mt-[-1.8em] md:px-4 font-semibold ">{`Bcard powers your community’s `}{<span className="text-[#08BFFF]">economic</span>} engine</h2>
                 <div>
                   <Image
                 src={"/images/image 22.svg"}
@@ -333,13 +333,13 @@ export default function LandingPage() {
                 alt=""
               /></div>
               </div>
-              <p className="hidden md:block text-center mt-4 leading-6">Using BCard, each member of your community creates recurring revenue with their existing everyday spending</p>
+              <p className="hidden md:block text-center mt-4 ">Using BCard, each member of your community creates recurring revenue with their existing everyday spending</p>
             </div>
 
 
             <div className="p-5 shadow-md gradient-bg-green-purple rounded-xl h-[100%] w-[100%]">
               <div className="flex flex-col-reverse md:grid md:grid-cols-[70%,30%] pt-4 gap-2">
-                <h2 className="text-[1.6em] max-sm:mt-5 md:text-[1.4em] md:px-4 font-semibold leading-8"><span className="text-[#14FF00]">Fuelling</span> projects and aspirations with every swipe</h2>
+                <h2 className="text-[1.6em] max-sm:mt-5 md:text-[1.4em] md:px-4 font-semibold "><span className="text-[#14FF00]">Fuelling</span> projects and aspirations with every swipe</h2>
                 <div className="max-sm:flex max-sm:gap-[1.5em] max-sm:justify-center">
                   <Image
                     src={"/images/arrow.svg"}
@@ -357,7 +357,7 @@ export default function LandingPage() {
                   />
               </div>
               </div>
-              <p className="hidden md:block text-center mt-4 leading-6">{`BCard uses payment card fees to buy your community’s tokens from a DEX, creating organic token demand.`}</p>
+              <p className="hidden md:block text-center mt-4 ">{`BCard uses payment card fees to buy your community’s tokens from a DEX, creating organic token demand.`}</p>
             </div>
            
           </div>
@@ -400,7 +400,7 @@ export default function LandingPage() {
 
 
         <section className="grid place-items-center  mt-[10em] mb-[5em]">
-        <h3 className="mb-[1.5em] text-[1.8em] font-semibold text-center leading-[2em]">Contact us</h3>
+        <h3 className="mb-[1.5em] text-[1.8em] font-semibold text-center ">Contact us</h3>
           <div className="flex flex-col gap-4 w-[90%] md:w-[65%] justify-center center-align">
             <div className="flex max-sm:flex-col max-sm:w-[200%] gap-4 w-[100%]">
             <input 
